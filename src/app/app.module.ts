712ab527afd18c643df47aa19e8330fa0fef7416
Benson Ly <bwly314@gmail.com>
2019-10-10 22:12:10 -0700

    Two basic tabs and loading bar have been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CreatePostingComponent } from './components/create-posting/create-posting.component';
 import { EditPostingComponent } from './components/edit-posting/edit-posting.component';
+import { IndexPostingComponent } from './components/index-posting/index-posting.component';
 
 import { RouterModule, Routes } from '@angular/router';
-import { IndexPostingComponent } from './components/index-posting/index-posting.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 const routes: Routes = [
   {
@@ -32,7 +33,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
