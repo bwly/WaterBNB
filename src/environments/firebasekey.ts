57d6466fb611714fe208da825f7d3b925d5d9c35
Benson Ly <bwly314@gmail.com>
2019-10-11 17:43:00 -0700

    Delete firebasekey.ts

--- src/environments/firebasekey.ts
+++
@@ -1,8 +0,0 @@
-export const firebasekey = {
-    apiKey: 'AIzaSyCQF5KM8REAsQTdT3fScLmRPrugwu_2Hdk',
-    authDomain: 'waterbnb-1c429.firebaseapp.com',
-    databaseURL: 'https://waterbnb-1c429.firebaseio.com',
-    projectId: 'waterbnb-1c429',
-    storageBucket: 'waterbnb-1c429.appspot.com',
-    messagingSenderId: '806224651860'
-};
